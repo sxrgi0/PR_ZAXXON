2ddaf465a743c6f3dfcda9312926f3ee6afd099d
Sergio Fernández <sergiofh.2002@gmail.com>
2021-10-05 13:00:38 +0200

    Restricción movimiento
    Movimiento restringido en todos los ejes.

--- PR_ZAXXON_FernandezSergio/Assets/Scripts/MovimientoNave.cs
+++ PR_ZAXXON_FernandezSergio/Assets/Scripts/MovimientoNave.cs
@@ -10,34 +10,62 @@ public class MovimientoNave : MonoBehaviour
     [SerializeField] float speed;
     float limiteR = 8;
     float limiteL = -8;
+    float limiteDown = 0.9f;
+    float limiteUp = 15;
+    bool inlimitH = true;
+    bool inlimitV = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        movimiento();
+
+
+    }
+
+    void movimiento()
+    {
         float posX = transform.position.x;
         float posY = transform.position.y;
 
         despX = Input.GetAxis("Horizontal");
-        transform.Translate (Vector3.right * despX * speed * Time.deltaTime);
-        
         despY = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.up * despY * speed * Time.deltaTime);
 
-       if(posX > limiteR && despX > 0)
+        if (inlimitH)
         {
-            speed = 0f;
+            transform.Translate(Vector3.right * despX * speed * Time.deltaTime);
+        }
+
+        if (inlimitV)
+        {
+            transform.Translate(Vector3.up * despY * speed * Time.deltaTime);
+        }
+
+
+
+        if (posX > limiteR && despX > 0 || posX < limiteL && despX < 0)
+        {
+            inlimitH = false;
         }
         else
         {
-            speed = 15f;
+            inlimitH = true;
         }
-
+        if (posY > limiteUp && despY > 0 || posY < limiteDown && despY < 0)
+        {
+            inlimitV = false;
+        }
+        else
+        {
+            inlimitV = true;
+        }
     }
+    
 }
