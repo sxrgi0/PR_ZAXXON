ea43df86421e205ed542e6cb658a1001ff25ae19
Sergio Fernández <sergiofh.2002@gmail.com>
2021-11-09 14:11:19 +0100

    User Interface
    Inicio User Interface, no está terminada.
    Menu principal y menu opciones

--- PR_ZAXXON_FernandezSergio/Assets/Scripts/InitGame.cs
+++ PR_ZAXXON_FernandezSergio/Assets/Scripts/InitGame.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InitGame : MonoBehaviour
 {
 
     public float spaceshipspeed;
     public int levelGame;
+    static float score;
+
+    [SerializeField] Text scoreText;
+    [SerializeField] Text levelText;
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +25,29 @@ public class InitGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Text();
     }
+
+    void Text()
+    {
+        float tiempo = Time.time;
+
+        score = Mathf.Round(tiempo) * spaceshipspeed;
+        scoreText.text = Mathf.Round(score) + " mts.";
+        levelText.text = "LEVEL: " + levelGame.ToString();
+        if (score > 0 && score < 500)
+        {
+            levelGame = 1;
+        }
+        else if (score > 500)
+        {
+            levelGame = 2;
+        }
+
+
+    }
+
+
 
     public void Morir()
     {
---
+++ PR_ZAXXON_FernandezSergio/Assets/Scripts/UIButtons.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtons : MonoBehaviour
+{
+
+   public void LoadScene (int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
